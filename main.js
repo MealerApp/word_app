0df7bd24bb3e89de66512e76a69d7132b95ddfab
Jayson Isaac <jayson.isaac1@gmail.com>
2013-09-28 16:47:23 -0400

    changed

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
 	$('#startGame').animate({top: "100px"},1000);
 });
+function closeGameDialog(){
+	var gameCode = "e12fwre4"
+	var inHTML = gameCode;
+	inHTML += "<br/>";
+	inHTML += "Enter this code into your device"
+	inHTML += "<br><br>"
+	inHTML += "<button onclick='closeDialog()' class='btn btn-success'>Ready?</button>"
+	$('#startGame').html(inHTML);
+}
+function closeDialog(){
+	$('#startGame').fadeOut();
+}
