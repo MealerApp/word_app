90d0a80602e2d2a65492e030b7e57f0de80c5516
Jayson Isaac <jayson.isaac1@gmail.com>
2013-09-28 16:51:53 -0400

    changed

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
 	$('#startGame').animate({top: "100px"},1000);
 });
 function closeGameDialog(){
-	var gameCode = "e12fwre4"
+	var gameCode = "e12fwre4";
+	$.ajax("http://lettershack.herokuapp.com/api/games/start", {
+		success: function(response){
+			gameCode = response;
+		}
+	});
 	var inHTML = gameCode;
 	inHTML += "<br/>";
 	inHTML += "Enter this code into your device"
